ed1be758f2ded55614e342dd42a4e59d9a48dcbe
Martin Rosas <mlrosas21@hotmail.com>
2023-10-18 20:13:23 -0300

    adding and fixing aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +8,8 @@ export default defineConfig({
       { find: "@", replacement: "/src" }, 
       { find: "@assets", replacement: "/src/assets" }, 
       { find: "@components", replacement: "/src/components" }, 
+      { find: "@data", replacement: "/src/data" },
+      { find: "@styles", replacement: "/src/styles" }
     ],
   },
   plugins: [react()],

af88788b1281009bec0b630c60e8635474a0a6bf
Martin Rosas <mlrosas21@hotmail.com>
2023-10-19 20:49:56 -0300

    adding dummy breadcrumbs

---
+++ client/src/components/SearchResults/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,11 @@
+import './Breadcrumbs.scss'
+
+function Breadcrumbs() {
+  return (
+    <div className='breadcrumbs'>
+      adasas - dadsda - dadas
+    </div>
+  )
+}
+
+export default Breadcrumbs

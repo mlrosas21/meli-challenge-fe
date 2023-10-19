b3c67f0bfd4be0b5e0e6f7c5685a4b7574dd6632
Martin Rosas <mlrosas21@hotmail.com>
2023-10-19 20:50:43 -0300

    search results page

--- client/src/components/SearchResults/SearchItem/SearchItem.jsx
+++ client/src/components/SearchResults/SearchItem/SearchItem.jsx
@@ -1,7 +1,25 @@
-function SearchItem() {
+import "./SearchItem.scss";
+import { formatMoney } from '@utils/formatter'
+
+function SearchItem({ item }) {
+  const { id, title, price, thumbnail, address } = item;
+
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
   return (
-    <div>SearchItem</div>
-  )
+    <article className="itemCard" onClick={() => handleClick(id)}>
+      <div className="imageContainer">
+        <img src={thumbnail} alt={title} />
+      </div>
+      <div className="itemMainInfo">
+        <p className="itemPrice">$ {formatMoney(price)}</p>
+        <p className="itemName">{title}</p>
+      </div>
+      <p className="stateName">{address.state_name}</p>
+    </article>
+  );
 }
 
-export default SearchItem
+export default SearchItem;
--- client/src/components/SearchResults/SearchResults.jsx
+++ client/src/components/SearchResults/SearchResults.jsx
@@ -1,6 +1,37 @@
+import './SearchResults.scss'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { getItems } from '@service/items'
+import SearchItem from './SearchItem/SearchItem'
+import Breadcrumbs from './Breadcrumbs/Breadcrumbs'
+
 function SearchResults() {
+  const [searchParams] = useSearchParams()
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const query = searchParams.get('search')
+    if (!query) return;
+
+    const fetchItems = async () => {
+      try {
+        const { results } = await getItems(query)
+        setItems(results)
+      } catch (error) {
+        console.error('Error fetching items:', error)
+      }
+    }
+
+    fetchItems()
+  }, [searchParams])
+
   return (
-    <div>SearchResults</div>
+    <div className='container'>
+      <Breadcrumbs />
+      <ul className='itemsList'>
+        {items.length > 0 && items.map(item => <li key={item.id}> <SearchItem  item={item}/> </li>)}
+      </ul>
+    </div>
   )
 }
 

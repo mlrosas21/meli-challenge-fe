75b8efae8d3f5a6126002521935691f23491a444
Martin Rosas <mlrosas21@hotmail.com>
2023-10-20 15:28:30 -0300

    redirect to home if invalid route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "@styles/globals.scss";
 import Header from "@components/Header/Header";
 import SearchResults from "@components/SearchResults/SearchResults";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" />
         <Route path="/items" element={<SearchResults />} />
         <Route path="/items/:id" element={<ProductDetails />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </main>
   );
